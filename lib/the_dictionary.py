50648df02c40e8a88e84d0a1ce26a4a42f237324
Sam <samer.zeid@sjsu.edu>
2017-03-04 17:20:15 -0800

    Dictionary
    Change the bot inputs/responses

---
+++ lib/the_dictionary.py
@@ -0,0 +1,7 @@
+#Add as many as you like
+
+check_dict = {
+            'Hey' : "What's up?", # Input, Response
+            "How's it going?" : '"Not bad, and you?",
+            'Input #3' : 'Response #4',
+        }
